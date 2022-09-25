5bd058cfbcbc33d4f06b33c909839290033df1c8
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-09-25 13:07:14 +0200

    Update damage source for cheap translations

--- common/src/main/java/net/spelldamage/api/SpellDamageSource.java
+++ common/src/main/java/net/spelldamage/api/SpellDamageSource.java
@@ -1,14 +1,26 @@
 package net.spelldamage.api;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.EntityDamageSource;
+import net.minecraft.entity.damage.ProjectileDamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import org.jetbrains.annotations.Nullable;
 
 import static net.spelldamage.api.MagicSchool.FIRE;
 
 public class SpellDamageSource extends EntityDamageSource {
-    public static SpellDamageSource create(MagicSchool school, Entity source) {
-        var damageSource = new SpellDamageSource(school.spellName(), source);
+    public static DamageSource mob(MagicSchool school, LivingEntity attacker) {
+        return SpellDamageSource.create(school, "mob", attacker);
+    }
+
+    public static DamageSource player(MagicSchool school, PlayerEntity attacker) {
+        return SpellDamageSource.create(school, "player", attacker);
+    }
+
+    public static SpellDamageSource create(MagicSchool school, String name, Entity source) {
+        var damageSource = new SpellDamageSource(name, source);
         damageSource.setUsesMagic();
         if (school == FIRE) {
             damageSource.setFire();
