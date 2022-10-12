ab71948b4278539782fcbe0150aa0cc2da24d9a0
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-10-12 12:18:09 +0200

    School filtered enchantment applied only to relevant item

---
+++ common/src/main/java/net/spelldamage/api/MagicalItemStack.java
@@ -0,0 +1,8 @@
+package net.spelldamage.api;
+
+import org.jetbrains.annotations.Nullable;
+
+public interface MagicalItemStack {
+    @Nullable
+    MagicSchool getMagicSchool();
+}
--- common/src/main/java/net/spelldamage/internals/SchoolFilteredEnchantment.java
+++ common/src/main/java/net/spelldamage/internals/SchoolFilteredEnchantment.java
@@ -2,7 +2,9 @@ package net.spelldamage.internals;
 
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.spelldamage.api.MagicSchool;
+import net.spelldamage.api.MagicalItemStack;
 import net.tinyconfig.models.EnchantmentConfig;
 
 import java.util.EnumSet;
@@ -17,5 +19,16 @@ public class SchoolFilteredEnchantment extends AmplifierEnchantment {
     public SchoolFilteredEnchantment(Rarity weight, Operation operation, EnchantmentConfig config, EnumSet<MagicSchool> schools, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
         super(weight, operation, config, type, slotTypes);
         this.schools = schools;
+    }
+
+    public boolean isAcceptableItem(ItemStack stack) {
+        var object = (Object)stack;
+        if (object instanceof MagicalItemStack magicalItemStack) {
+            var school = magicalItemStack.getMagicSchool();
+            if (school != null) {
+                return schools.contains(school);
+            }
+        }
+        return false;
     }
 }
