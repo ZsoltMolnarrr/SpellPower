38eb0c7b2fe3d28518b375e9a542fcabd46e23e4
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-01-08 01:13:01 +0100

    Disable energize

--- common/src/main/java/net/spell_power/config/EnchantmentsConfig.java
+++ common/src/main/java/net/spell_power/config/EnchantmentsConfig.java
@@ -15,7 +15,8 @@ public class EnchantmentsConfig {
     public ExtendedEnchantmentConfig spell_power = new ExtendedEnchantmentConfig(MAGICAL_WEAPON,5, 10, 9, 0.1F);
     public ExtendedEnchantmentConfig soulfrost = new ExtendedEnchantmentConfig(MAGICAL_ARMOR, 5, 10, 9, 0.1F);
     public ExtendedEnchantmentConfig sunfire = new ExtendedEnchantmentConfig(MAGICAL_ARMOR,5, 10, 9, 0.1F);
-    public ExtendedEnchantmentConfig energize = new ExtendedEnchantmentConfig(MAGICAL_ARMOR,5, 10, 9, 0.1F);
+    public ExtendedEnchantmentConfig energize = new ExtendedEnchantmentConfig(MAGICAL_ARMOR,5, 10, 9, 0.1F)
+            .setEnabled(false); // Remove when healer content is added
     public ExtendedEnchantmentConfig critical_chance = new ExtendedEnchantmentConfig(MAGICAL_ARMOR, 5, 10, 12, 0.05F);
     public ExtendedEnchantmentConfig critical_damage = new ExtendedEnchantmentConfig(MAGICAL_ARMOR, 5, 10, 12, 0.1F);
     public ExtendedEnchantmentConfig haste = new ExtendedEnchantmentConfig(MAGICAL_WEAPON, 5, 15, 17, 0.05F);
@@ -40,6 +41,11 @@ public class EnchantmentsConfig {
         public ExtendedEnchantmentConfig(ItemType requires, int max_level, int min_cost, int step_cost, float bonus_per_level) {
             super(max_level, min_cost, step_cost, bonus_per_level);
             this.requires = requires;
+        }
+
+        public ExtendedEnchantmentConfig setEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
         }
     }
 }
