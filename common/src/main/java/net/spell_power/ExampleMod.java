1f9e7ef8f795dc6a4f5a15f5e4e42220cf0610f7
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-11 19:25:25 +0200

    Add files from new project template

---
+++ common/src/main/java/net/spell_power/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.spell_power;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "spell_power";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
